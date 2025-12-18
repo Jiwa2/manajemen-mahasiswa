d26abd1594e0dbb92819d15d6f06392c7fff88a9
Jiwa2 <mjiwaislamutidar@gmail.com>
2025-12-18 10:45:27 +0700

    fix railways

--- backend/server.cjs
+++ backend/server.cjs
@@ -131,7 +131,9 @@ app.get("/api/mahasiswa/search/:nim", (req, res) => {
 
 /* ================= PORT (INI KUNCI RAILWAY) ================= */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
+
 
